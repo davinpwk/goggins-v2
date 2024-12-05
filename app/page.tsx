9060790bb9176949375b6d52403c5fd4c6a19857
davinpwk <davinpwk@gmail.com>
2024-12-05 17:02:30 +0700

    Implement persona selector, and form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,71 @@
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { persona } from "@/constants/persona";
+
+interface Persona {
+  id: number;
+  name: string;
+  problem: string;
+  req: string;
+}
+
 export default function Home() {
-  return <div>Home</div>
+  const [activePersona, setActivePersona] = useState<Persona>(persona[0]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const logData = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="bg-background-light flex flex-col items-center justify-start gap-4 rounded-lg p-10 shadow-lg">
+      <h1 className="h1-bold">Davin Goggins v2</h1>
+      <div className="flex w-full justify-between gap-3">
+        {persona.map((person) => {
+          return (
+            <Button
+              key={person.id}
+              onClick={() => setActivePersona(person)}
+              className={`${activePersona.id === person.id ? "active" : ""}`}
+            >
+              {person.name}
+            </Button>
+          );
+        })}
+      </div>
+
+      <form
+        onSubmit={handleSubmit(logData)}
+        className="flex w-full flex-col gap-4"
+      >
+        <Input
+          id="name"
+          type="text"
+          placeholder="NAME"
+          className="border-none p-5 shadow-md"
+          {...register("name", { required: "Name is required" })}
+        />
+        <Textarea
+          id="problem"
+          placeholder={activePersona.problem}
+          className="border-none p-5 shadow-md"
+          {...register("problem", { required: "Problem is required" })}
+        />
+        <Button type="submit" className="active mt-4 p-5 shadow-md">
+          {activePersona.req}
+        </Button>
+      </form>
+    </div>
+  );
 }
---
+++ constants/persona.ts
@@ -0,0 +1,26 @@
+export const persona = [
+  {
+    id: 0,
+    name: "Goggins",
+    problem: "What's the problem SON",
+    req: "Enlighten me Goggins",
+  },
+  {
+    id: 1,
+    name: "Sun Tzu",
+    problem: "What battle do you face, my soldier",
+    req: "Wise General, share me your counsel",
+  },
+  {
+    id: 2,
+    name: "Marcus Aurelius",
+    problem: "What disturbance of the mind do you face?",
+    req: "Share your wisdom, Caesar.",
+  },
+  {
+    id: 3,
+    name: "Kak Gem",
+    problem: "Ada apa DEK?",
+    req: "KATA-KATANYA DONG KAK GEM!!",
+  },
+];
